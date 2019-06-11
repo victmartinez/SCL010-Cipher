c23b4468397e4f5546655d935f1cb295c7ec15ef
victmartinez <victmartinez@outlook.com>
2019-06-11 11:41:10 -0400

    Ideas JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,19 @@
+//rescatar el valor del offset
+// traer value de la caja de texto toCipher
+// crear botones con diferentes funciones de cifrar y descifrar
+// ejecutar fórmula michelle;
+        //hacer un for que recorra el toCipher.value y convierta según el offset el array
+        // aplicar .charCodeAt(i del for)-numerocodigoascci y .fromCharCode(#i)-me retorna la letra del numero del asccii  
+        //retornar el array creado  
+        //+= (.push) y guardarlo en dechiper
+        //voilá
+        //funciones para mayúsculas, minúsculas, espacios, y número.
+//buscar enlaces de ayuda
+        //hacer botones con enlaces de ayuda<a>
+//css
+//readme
+// (posiciondelaletraenascci + offset) % 26(totaldeletras) + 65 = nueva posicion en el codigo asccii
+
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
