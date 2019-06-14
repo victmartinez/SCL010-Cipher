73d5ffb8085a913fbaf964f176d154dca65c82c5
victmartinez <victmartinez@outlook.com>
2019-06-13 22:53:06 -0400

    se cambio input por text area, pruebas de dom con consolelog.

--- src/cipher.js
+++ src/cipher.js
@@ -4,7 +4,7 @@
 // ejecutar fórmula michelle;
         //hacer un for que recorra el toCipher.value y convierta según el offset el array
         // aplicar .charCodeAt(i del for)-numerocodigoascci y .fromCharCode(#i)-me retorna la letra del numero del asccii  
-        //retornar el array creado  
+        //retornar el array creado ES STRING NO ARRAY AAAAAAAAAAAAAAAAAAAAA
         //+= (.push) y guardarlo en dechiper
         //voilá
         //funciones para mayúsculas, minúsculas, espacios, y número.
@@ -14,14 +14,31 @@
 //readme
 // (posiciondelaletraenascci + offset) % 26(totaldeletras) + 65 = nueva posicion en el codigo asccii
 
+
 window.cipher = {
   encode: (offset, textToEncode) => {
-    
-      },
+    let encoded = "";
+    let asciiCode= "";
+  //se recorre el string textToEncode y le aplicamos a cada posición charcodeat para tomar el numero de ubicacion en ascii
+    for (let i = 0; i<textToEncode.length; i++) {
+      asciiCode = textToEncode[i].charCodeAt();
+   //MINUSCULAS
+      if (asciiCode >= 97 && asciiCode <= 122) {
+        //Tomamos los valores de Ascii Code y los convertimos de nuevo enletras
+        encoded += String.fromCharCode((asciiCode - 97 + offset) % 26 + 97);
+        // resultado
+ {
+        encoded += textToEncode[i];
+      }
+    }
+    return encoded;
+      // console.log(encoded);
+    }
+  }};
 /* Acá va tu código */
     
  
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+//   decode: () => {
+//     /* Acá va tu código */
+//   }
+// },
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+
+
 // selección cifrar  
 const encodeBtn = document.getElementById("botonEncodeHTML");
-encodeBtn.addEventListener("click", () => {
-    let textToEncode = document.getElementById("toEncodeHTML").value;
-    let offset= document.getElementById("offsetHTML").value;
-    // impresion
-    document.getElementById("resultHTML").innerHTML(window.cipher.encode(textToEncode, offset));    
-});
-//selección descifrar
-const decodeBtn = document.getElementById("botondecodeHTML");
-encodeBtn.addEventListener("click", () => {
-    let textToDecode = document.getElementById("toEncodeHTML").value;
-    let offset = document.getElementById("offsetHTML").value;
-    // impresion
-    document.getElementById("resultHTML").innerHTML(window.cipher.decode(textToDecode, offset));
-});
 
-//agregar boton limpiar ()
+        encodeBtn.addEventListener("click", () => {
+            let textToEncode = document.getElementById("toEncodeHTML").value;
+            let offset = document.getElementById("offsetHTML").value;
+            // console.log(textToEncode);
+            // console.log(offset);
+
+            // impresion
+            document.getElementById("resultHTML").innerHTML = window.cipher.encode(offset, textToEncode);
+        });
+        
+            // console.log(solution);
+       
+
+
+
+//selección descifrar
+// const decodeBtn = document.getElementById("botondecodeHTML");
+//         decodeBtn.addEventListener("click", () => {
+//             let textToDecode = document.getElementById("toEncodeHTML").value;
+//             let offset = document.getElementById("offsetHTML").value;
+//             // impresion iwal que arriba
+//             solution = window.cipher.decode(offset, textToDecode);
+//             document.getElementById("resultHTML").innerHTML = solution;
+//             // console.log(solution);
+//         });
+
+        //agregar boton limpiar ()
